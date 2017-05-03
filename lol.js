3dee3777713b886a3dd456a32930a6e80ffdd731
VovaGrigorev <grigorevv191@gmail.com>
2017-05-03 22:25:21 +0300

    first commit

---
+++ lol.js
@@ -0,0 +1,35 @@
+var name = prompt('your name?');
+
+console.log('імя: ',name);
+
+
+var firstNumber = Number(prompt('Enter first number'));
+var secondNumber = Number(prompt('Enter second number'));
+
+console.log('First number: ',firstNumber);
+console.log('Second number: ',secondNumber);
+
+var add = firstNumber + secondNumber;
+var sub = firstNumber - secondNumber;
+var mult = firstNumber * secondNumber;
+var div = firstNumber / secondNumber;
+var mod = firstNumber % secondNumber;
+var inc = ++firstNumber;
+var dec = --secondNumber;
+
+console.log('add',add);
+console.log('sub',sub);
+console.log('mult',mult);
+console.log('div',div);
+console.log('mod',mod);
+console.log('inc',inc);
+console.log('dec',dec);
+
+var x = Number(prompt('enter x'));
+var y = Number(prompt('enter y'));
+var z = Number(prompt('enter z'));
+console.log(x + (y*z));
+
+console.log('enter x', x + (y*z));
+console.log('enter y', x + (y*z));
+console.log('enter z', x + (y*z));
